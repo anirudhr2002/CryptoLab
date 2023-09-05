288ee9db9aba6a1f16bceb77851fabf02c04356a
anirudhr2002 <144113199+anirudhr2002@users.noreply.github.com>
2023-09-05 15:56:57 +0530

    Update PolyCipher.java

--- PolyCipher.java
+++ PolyCipher.java
@@ -1,74 +1,98 @@
-
-import java.io.*;
-import java.net.*;
 import java.util.*;
-class PolyCipher {
 
-    static String generateKey(String str, String key) {
-        int x = str.length();
+class PolyCipher
+{
 
-        for (int i = 0;; i++) {
-            if (x == i) {
-                i = 0;
-            }
-            if (key.length() == str.length()) {
-                break;
-            }
-            key += (key.charAt(i));
-        }
-        return key;
-    }
+static String generateKey(String str, String key)
+{
+	int x = str.length();
 
-    static String cipherText(String str, String key) {
-        String cipher_text = "";
-
-        for (int i = 0; i < str.length(); i++) {
-            // converting in range 0-25
-            int x = (str.charAt(i) + key.charAt(i)) % 26;
-
-            // convert into alphabets(ASCII)
-            x += 'A';
-
-            cipher_text += (char) (x);
-        }
-        return cipher_text;
-    }
-    static String LowerToUpper(String s) {
-        StringBuffer str = new StringBuffer(s);
-        for (int i = 0; i < s.length(); i++) {
-            if (Character.isLowerCase(s.charAt(i))) {
-                str.setCharAt(i, Character.toUpperCase(s.charAt(i)));
-            }
-        }
-        s = str.toString();
-        return s;
-    }
-// Driver code
-    public static void main(String[] args) {
-    try {
-            System.out.print("Plain Text : ");
-            Scanner sc = new Scanner(System.in);
-            String plain = sc.nextLine();
-            plain = LowerToUpper(plain);
-            
-            System.out.print("Key : ");
-            String key = sc.nextLine();
-            key = generateKey(plain, key);
-            key = LowerToUpper(key);
-            String cipher = cipherText(plain, key);
-            System.out.println("Cipher Text:" + cipher);
-            Socket s = new Socket("localhost", 6667);
-            DataOutputStream dout = new DataOutputStream(s.getOutputStream());
-            dout.writeUTF(cipher);
-            dout.flush();
-            dout.writeUTF(key);
-            dout.close();
-            s.close();
-            sc.close();
-        } catch (Exception e) {
-            System.out.println(e);
-        }
-    }
+	for (int i = 0; ; i++)
+	{
+		if (x == i)
+			i = 0;
+		if (key.length() == str.length())
+			break;
+		key+=(key.charAt(i));
+	}
+	return key;
 }
 
-// This code has been contributed by 29AjayKumar
+
+static String cipherText(String str, String key)
+{
+	String cipher_text="";
+
+	for (int i = 0; i < str.length(); i++)
+	{
+
+		int x = (str.charAt(i) + key.charAt(i)) %26;
+
+
+		x += 'A';
+
+		cipher_text+=(char)(x);
+	}
+	return cipher_text;
+}
+
+
+static String originalText(String cipher_text, String key)
+{
+	String orig_text="";
+
+	for (int i = 0 ; i < cipher_text.length() &&
+							i < key.length(); i++)
+	{
+
+		int x = (cipher_text.charAt(i) -
+					key.charAt(i) + 26) %26;
+
+
+		x += 'A';
+		orig_text+=(char)(x);
+	}
+	return orig_text;
+}
+
+
+static String LowerToUpper(String s)
+{
+	StringBuffer str =new StringBuffer(s);
+	for(int i = 0; i < s.length(); i++)
+	{
+		if(Character.isLowerCase(s.charAt(i)))
+		{
+			str.setCharAt(i, Character.toUpperCase(s.charAt(i)));
+		}
+	}
+	s = str.toString();
+	return s;
+}
+
+
+public static void main(String[] args)
+{
+	String Str;
+	String Keyword;
+	
+	Scanner cin = new Scanner(System.in);
+	System.out.println("Enter Text: ");
+	Str = cin.nextLine();
+	System.out.println("Enter Keyword: ");
+	Keyword = cin.nextLine();
+	
+	String str = LowerToUpper(Str);
+	String keyword = LowerToUpper(Keyword);
+
+	String key = generateKey(str, keyword);
+	String cipher_text = cipherText(str, key);
+
+	System.out.println("Ciphertext : "
+		+ cipher_text + "\n");
+
+	System.out.println("Original/Decrypted Text : "
+		+ originalText(cipher_text, key));
+	}
+}
+
